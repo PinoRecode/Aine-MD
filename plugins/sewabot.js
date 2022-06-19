f28d51803c754a5a4df3c874b3c6cdfc3d78e5d7
Who Is Pino? <87032037+PinoRecode@users.noreply.github.com>
2022-06-19 10:35:15 +0700

    update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -13,15 +13,15 @@ let handler = async (m, { conn }) => {
   description: ` *• SEWA BOT & UP TO PREMIUM •*
         
 1. Grup / 30 Hari
-Rp. 15.000 Dana
+Rp. 15.000 Gopay
 Rp. 15.000 Pulsa
 
 2. Premium / 30 Hari
-Rp. 10.000 Dana
+Rp. 10.000 Gopay
 Rp. 10.000 Pulsa
 
 3. Premium + Grup / 30 Hari
-Rp. 20.000 Dana
+Rp. 20.000 Gopay
 Rp. 20.000 Pulsa
 
 wa.me/${owner[0]}
@@ -36,9 +36,9 @@ wa.me/${owner[0]}
   products:[{productId:'7487741964584083'}]}],
   headerImage: { productId: '7487741964584083',
   jpegThumbnail: baper },
-  businessOwnerJid: `6289654360447@s.whatsapp.net`
+  businessOwnerJid: `628895377665@s.whatsapp.net`
   },
-  footerText: 'https://aiinne.github.io',
+  footerText: 'http://pinowebdev.coolpage.biz',
   }},{})
   conn.relayMessage(prepare.key.remoteJid,prepare.message,{messageId:prepare.key.id})
   const data = global.owner.filter(([id, isCreator]) => id && isCreator)
