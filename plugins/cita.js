e9f9975a51089cea2fdf3de0a6c1fa2a8c54930a
Who Is Pino? <87032037+PinoRecode@users.noreply.github.com>
2022-06-21 05:11:06 +0700

    Update cita.js

--- plugins/cita.js
+++ plugins/cita.js
@@ -1,6 +1,6 @@
 let fetch = require("node-fetch")
 let arr = []
-fetch('https://raw.githubusercontent.com/AlvioAdjiJanuar/citacita/main/citacita.txt')
+fetch('https://raw.githubusercontent.com/nizambot/cita/main/PsycoBOTZ.txt')
   .then(res => res.text())
   .then(txt => arr = txt.split('\n'))
 let handler = async (m, { conn }) => {
